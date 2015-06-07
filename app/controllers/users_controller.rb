dd032260da142f8a9c607f6b178fbe1cf7a45f15
Kelly Witwicki Faddegon <kelly.witwickifaddegon@gmail.com>
2015-06-07 11:47:46 -0700

    Clean debugging prints out of users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      p "SAVED #{@user.username}"
       sign_in(@user)
     else
-      p "ERRORSSSSSSSSSSS"
-      p @user.errors.full_messages
       flash.now[:errors] = @user.errors.full_messages
       render :new
     end
