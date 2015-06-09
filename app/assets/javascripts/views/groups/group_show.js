1e5d1a0fe2683e5b4a9e9171d774e522316b2370
Kelly Witwicki Faddegon <kelly.witwickifaddegon@gmail.com>
2015-06-09 13:27:00 -0700

    Cleanup

--- app/assets/javascripts/views/groups/group_show.js
+++ app/assets/javascripts/views/groups/group_show.js
@@ -3,22 +3,9 @@ SuperSocietyApp.Views.GroupShow = Backbone.CompositeView.extend({
 
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
-
-    // toggle for current event id
-    // in render, f current event id, render that single subview, else render index
-    // clicking an event sets current event id, rerenders entire view
-    console.log(this.model);
     this._modelEvents = this.model.ssevents();
     this._modelEvents.fetch();
-    // this.model.events().fetch() // fetch each??
     this.listenTo(this._modelEvents, "sync", this.addEventsIndexSubview);
-
-    // SuperSocietyApp.events.fetch({
-    //   success: function () {
-    //     this._events = SuperSocietyApp.events.where({ group_id: this.model.id });
-    //     this.addEventsIndexSubview();
-    //   }.bind(this)
-    // });
   },
 
   addEventsIndexSubview: function () {
@@ -44,14 +31,6 @@ SuperSocietyApp.Views.GroupShow = Backbone.CompositeView.extend({
     this.$el.html(this.template({ group: this.model }));
     return this;
   },
-
-  // _swapSubview: function (view) {
-  //   if (this._currentSubview) {
-  //     this.removeSubview(".events", this._currentSubview);
-  //   }
-  //   this._currentSubview = view;
-  // }
-
   _swapSubview: function (view) {
     if (this._currentSubview) {
       this._currentSubview.remove();
