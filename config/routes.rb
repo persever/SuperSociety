8fc3f190905732e4791252ac2203f159a687f0bc
Kelly Witwicki Faddegon <kelly.witwickifaddegon@gmail.com>
2015-06-09 13:26:21 -0700

    Make custom entry route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#index'
 
   resource :session, only: [:destroy]
-  resources :users, only: [:new, :create], path: "bifrost"
+  resources :users, only: [:new, :create], path: "welcome"
 
   namespace :api, defaults: { format: :json } do
     resources :groups
