9b12368ae3440f330be39f3401fe593415c55908
Kelly Witwicki Faddegon <kelly.witwickifaddegon@gmail.com>
2015-06-18 12:15:08 -0700

    Fix event index item slide conditionals

--- app/assets/javascripts/views/events/events_index.js
+++ app/assets/javascripts/views/events/events_index.js
@@ -18,20 +18,21 @@ SuperSocietyApp.Views.EventsIndex = Backbone.View.extend({
   },
 
   slideItems: function (ssevent) {
-    console.log("here");
     ssevent = ssevent || this.collection.models[0];
     var now = new Date().getTime();
     var time = new Date(now);
     var datetime = new Date(ssevent.get("datetime"));
+    var item = new SuperSocietyApp.Views.EventsIndexItem({ model: ssevent });
+    var $item = item.render().$el;
+    $item.addClass("bounceInRight");
     if (datetime > time) {
-      var item = new SuperSocietyApp.Views.EventsIndexItem({ model: ssevent });
-      var $item = item.render().$el;
-      $item.addClass("bounceInRight");
       setTimeout(function () {
         this.$el.append($item);
-        if (ssevent !== this.collection.models[this.collection.length - 1]) {
-          this.slideItems(this.collection.models[this.collection.models.indexOf(ssevent) + 1]);
-        }
+      }.bind(this), 100);
+    }
+    if (ssevent !== this.collection.models[this.collection.length - 1]) {
+      setTimeout(function () {
+        this.slideItems(this.collection.models[this.collection.models.indexOf(ssevent) + 1]);
       }.bind(this), 100);
     }
   }
