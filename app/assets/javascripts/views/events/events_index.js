98bf118fbd5847a0b89b499967fcd3f7cec8d53f
Kelly Witwicki Faddegon <kelly.witwickifaddegon@gmail.com>
2015-06-18 10:22:29 -0700

    Event index items slide

--- app/assets/javascripts/views/events/events_index.js
+++ app/assets/javascripts/views/events/events_index.js
@@ -9,16 +9,29 @@ SuperSocietyApp.Views.EventsIndex = Backbone.View.extend({
 
   render: function () {
     this.$el.html(this.template());
-    this.collection.forEach(function (ssevent) {
-      var now = new Date().getTime();
-      var time = new Date(now);
-      var datetime = new Date(ssevent.get("datetime"));
-      if (datetime > time) {
-        var item = new SuperSocietyApp.Views.EventsIndexItem({ model: ssevent });
-        this.$el.append(item.render().$el);
-      }
-    }.bind(this));
+
+    if (this.collection.length > 0) {
+      this.slideItems();
+    }
 
     return this;
+  },
+
+  slideItems: function (ssevent) {
+    ssevent = ssevent || this.collection.models[0];
+    var now = new Date().getTime();
+    var time = new Date(now);
+    var datetime = new Date(ssevent.get("datetime"));
+    if (datetime < time) {
+      var item = new SuperSocietyApp.Views.EventsIndexItem({ model: ssevent });
+      var $item = item.render().$el;
+      $item.addClass("bounceInRight");
+      setTimeout(function () {
+        this.$el.append($item);
+        if (ssevent !== this.collection.models[this.collection.length - 1]) {
+          this.slideItems(this.collection.models[this.collection.models.indexOf(ssevent) + 1]);
+        }
+      }.bind(this), 100);
+    }
   }
 });
