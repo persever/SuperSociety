3f406e2d674809c67b599859fe5377222236ed8e
Kelly Witwicki Faddegon <kelly.witwickifaddegon@gmail.com>
2015-06-19 10:26:55 -0700

    Animate group index

--- app/assets/javascripts/views/groups/groups_index.js
+++ app/assets/javascripts/views/groups/groups_index.js
@@ -10,11 +10,35 @@ SuperSocietyApp.Views.GroupsIndex = Backbone.View.extend({
 
   render: function () {
     this.$el.html(this.template());
-    this.collection.forEach(function (group) {
-      var item = new SuperSocietyApp.Views.GroupsIndexItem({ model: group });
-      this.$el.append(item.render().$el);
-    }.bind(this));
+    // this.collection.forEach(function (group) {
+    //   var item = new SuperSocietyApp.Views.GroupsIndexItem({ model: group });
+    //   this.$el.append(item.render().$el);
+    // }.bind(this));
+
+    if (this.collection.length > 0) {
+      this.fadeIn();
+    }
 
     return this;
+  },
+
+  fadeIn: function (group) {
+    group = group || this.collection.models[0];
+    var item = new SuperSocietyApp.Views.GroupsIndexItem({ model: group });
+    var $item = item.render().$el;
+
+    if ($(".search-query").val()) {
+      this.$el.append($item);
+    } else {
+      $item.addClass("slideInUp");
+      setTimeout(function () {
+        this.$el.append($item);
+      }.bind(this), 100);
+      if (group !== this.collection.models[this.collection.length - 1]) {
+        setTimeout(function () {
+          this.fadeIn(this.collection.models[this.collection.models.indexOf(group) + 1]);
+        }.bind(this), 100);
+      }
+    }
   }
 });
